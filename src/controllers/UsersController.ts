1e00c630f9ae69e00d4483d138f10254a6059b1b
ytalo azeredo <ytaloazeredo@gmail.com>
2022-08-18 00:11:25 -0300

    fix: controller returns

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -4,58 +4,38 @@ import ConflictError from '@errors/ConflictError'
 import UserRepository from '@repositories/UserRepository'
 
 class UsersController {
-  async getAll (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
-    try {
-      const users = await UserRepository.getAll({ throws: true })
-      return res.json(users)
-    } catch (err) {
-      next(err)
-    }
+  async getAll (req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const users = await UserRepository.getAll({ throws: true })
+    return res.json({ response: users })
   }
 
-  async get (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
-    try {
-      const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
-      return res.json(user)
-    } catch (err) {
-      next(err)
-    }
+  async get (req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
+    return res.json({ response: user })
   }
 
-  async store (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
-    try {
-      const userData: Users = req.body
-      const userExist = await UserRepository.getOneBy({ email: userData.email })
-      if (userExist) throw new ConflictError(UserRepository.getConflictError())
-      const createdUser = await UserRepository.store(userData)
-      return res.json(createdUser)
-    } catch (err) {
-      next(err)
-    }
+  async store (req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const userData: Users = req.body
+    const userExist = await UserRepository.getOneBy({ email: userData.email })
+    if (userExist) throw new ConflictError(UserRepository.getConflictError())
+    const createdUser = await UserRepository.store(userData)
+    return res.json({ response: createdUser })
   }
 
-  async delete (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
-    try {
-      const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
-      await UserRepository.remove(user)
-      return res.json(UserRepository.getDeleteMessage())
-    } catch (err) {
-      next(err)
-    }
+  async delete (req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
+    await UserRepository.remove(user)
+    return res.json({ message: UserRepository.getDeleteMessage() })
   }
 
-  async update (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
-    try {
-      const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
-      const userData: Users = req.body
-      user.name = userData.name
-      user.email = userData.email
-      user.password = userData.password
-      await UserRepository.save(user)
-      return res.json(UserRepository.getUpdateMessage())
-    } catch (err) {
-      next(err)
-    }
+  async update (req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const user = await UserRepository.getOneBy({ id: +req.params.id, throws: true })
+    const userData: Users = req.body
+    user.name = userData.name
+    user.email = userData.email
+    user.password = userData.password
+    await UserRepository.save(user)
+    return res.json({ response: user })
   }
 };
 
