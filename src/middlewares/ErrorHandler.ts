cceacc831d9511fc449cb806289edcca657be4ea
ytalo azeredo <ytaloazeredo@gmail.com>
2022-08-18 00:11:32 -0300

    fix: error handler return

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -2,6 +2,6 @@ import { Request, Response, NextFunction } from 'express'
 
 export const errorHandler = async function (err: any, req: Request, res: Response, next: NextFunction) {
   if (err) {
-    return res.status(err.statusCode || 500).json(err.message)
+    return res.status(err.statusCode || 500).json({ message: err.message })
   }
 }
