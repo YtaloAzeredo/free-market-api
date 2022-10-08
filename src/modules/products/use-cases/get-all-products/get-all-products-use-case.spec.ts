4ece4d14b8dd679e9020f8e47b2c9466f36635ca
ytalo azeredo <ytaloazeredo@gmail.com>
2022-10-08 10:59:21 -0300

    refactor: get all products use case unit test

--- src/modules/products/use-cases/get-all-products/get-all-products-use-case.spec.ts
+++ src/modules/products/use-cases/get-all-products/get-all-products-use-case.spec.ts
@@ -1,5 +1,5 @@
-import { ProductsRepository } from '@modules/products/repositories/in-memory/products.repository';
-import { GetAllProductsUseCase } from './get-all-products.use-case';
+import { ProductsModel } from '@modules/products/models/products.model'
+import { GetAllProductsUseCase } from './get-all-products.use-case'
 const ZERO = 0
 const PRODUCTS_PROPS = [
   'description',
@@ -7,19 +7,16 @@ const PRODUCTS_PROPS = [
   'quantity'
 ]
 const ERROR_MESSAGE = 'Nenhum produto encontrado'
-
+let productsRepository: any, getAllProductsUseCase: GetAllProductsUseCase
 describe('GetAllProductsUseCase', () => {
-  let productsRepository: ProductsRepository, getAllProductsUseCase: GetAllProductsUseCase;
-  beforeEach(() => {
-    productsRepository = new ProductsRepository()
-    getAllProductsUseCase = new GetAllProductsUseCase(productsRepository)
-  })
+  beforeEach(() => makeSut())
   describe('Quando requisitar uma lista de produtos', () => {
     test('Deve retornar os dados dos produtos encontrados', async () => {
-      productsRepository.factory()
+      const repositorySpy = jest.spyOn(productsRepository, 'getAll')
 
       const response = await getAllProductsUseCase.execute()
 
+      expect(repositorySpy).toHaveBeenCalled()
       expect(response.length).toBeGreaterThan(ZERO)
       response.map(result => {
         PRODUCTS_PROPS.map(prop => {
@@ -31,26 +28,33 @@ describe('GetAllProductsUseCase', () => {
 
   describe('Quando requisitar uma lista de produtos e não encontrar nenhum registro', () => {
     test('Deve lançar uma exceção', async () => {
+      jest.spyOn(productsRepository, 'getAll').mockReturnValue([])
+
       const response = getAllProductsUseCase.execute()
-      
+
       await expect(response)
-      .rejects
-      .toThrow(ERROR_MESSAGE)
-    })
-  })
-
-  describe('Quando requisitar uma lista de produtos', () => {
-    test('Deve os dados retornados devem ser do tipo \'Products\'', async () => {
-      productsRepository.factory()
-
-      const response = await getAllProductsUseCase.execute()
-
-      expect(response.length).toBeGreaterThan(ZERO)
-      response.map(result => {
-        PRODUCTS_PROPS.map(prop => {
-          expect(result).toHaveProperty(prop)
-        })
-      })
+        .rejects
+        .toThrow(ERROR_MESSAGE)
     })
   })
 })
+
+const makeSut = () => {
+  productsRepository = makeRepository()
+  getAllProductsUseCase = new GetAllProductsUseCase(productsRepository)
+}
+
+const makeRepository = () => {
+  class ProductsRepositoryStub {
+    async getAll (): Promise<ProductsModel[]> {
+      return [
+        {
+          description: 'sut',
+          price: 1,
+          quantity: 1
+        }
+      ]
+    }
+  }
+  return new ProductsRepositoryStub()
+}
