52c815f53fa1b95cd9a2a5d8a303abc85616e68d
ytalo azeredo <ytaloazeredo@gmail.com>
2022-10-31 02:03:15 -0300

    test: get all products use case

--- src/modules/products/use-cases/get-all-products/get-all-products-use-case.spec.ts
+++ src/modules/products/use-cases/get-all-products/get-all-products-use-case.spec.ts
@@ -11,13 +11,33 @@ let productsRepository: any, getAllProductsUseCase: GetAllProductsUseCase
 describe('GetAllProductsUseCase', () => {
   beforeEach(() => makeSut())
   describe('Quando requisitar uma lista de produtos', () => {
-    test('Deve retornar os dados dos produtos encontrados', async () => {
+    test('O caso de uso deve ser instanciado corretamente', async () => {
+      expect(getAllProductsUseCase).toBeInstanceOf(GetAllProductsUseCase)
+    })
+  })
+
+  describe('Quando requisitar consultar o caso de uso de busca de produtos', () => {
+    test('O método getAll() do repositório deve ser chamado corretamente', async () => {
       const repositorySpy = jest.spyOn(productsRepository, 'getAll')
 
-      const response = await getAllProductsUseCase.execute()
+      await getAllProductsUseCase.execute()
 
       expect(repositorySpy).toHaveBeenCalled()
+    })
+  })
+
+  describe('Quando requisitar corretamente uma lista de produtos', () => {
+    test('Deve retornar os dados dos produtos encontrados', async () => {
+      const response = await getAllProductsUseCase.execute()
+
       expect(response.length).toBeGreaterThan(zero)
+    })
+  })
+
+  describe('Quando requisitar uma lista de produtos e retornar dados', () => {
+    test('Todas as propriedades dos dados devem ser correspondentes às propriedades de produto', async () => {
+      const response = await getAllProductsUseCase.execute()
+
       response.map(result => {
         productProps.map(prop => {
           expect(result).toHaveProperty(prop)
@@ -49,11 +69,21 @@ const makeRepository = () => {
     async getAll (): Promise<ProductsModel[]> {
       return [
         {
+          id: 1,
           description: 'sut',
           price: 1,
-          quantity: 1
+          quantity: 1,
+          category: {
+            id: 1,
+            code: 'eletronics',
+            description: 'Produtos Eletrônicos'
+          }
         }
       ]
+    }
+
+    getNotFoundMessage (): string {
+      return 'Nenhum produto encontrado'
     }
   }
   return new ProductsRepositoryStub()
